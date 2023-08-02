ae6eaa60fb33deb4e953e9a3cb6d3f345e5eae9e
Anna <29932164+alw3979@users.noreply.github.com>
2023-08-02 17:19:28 -0500

    complete 08 assignments, all tests passed!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,46 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  return num1 - num2
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  let runningTotal = 0
+
+  while (array.length > 0){
+    runningTotal = add(runningTotal, array.pop())
+  }
+  return runningTotal
 	
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let runningTotal = 1
+  while (array.length > 0){
+    runningTotal = runningTotal * array.pop()
+  }
+  return runningTotal
 };
 
-const power = function() {
+const power = function(num1, num2) {
+  let runningTotal = num1
+  for (num2; num2 > 1; num2--) {
+    runningTotal=runningTotal * num1
+  }
+  return runningTotal
 	
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+  if (num===0) {
+    return 1;
+  }
+  return num * factorial(num-1)
+}
 
 // Do not edit below this line
 module.exports = {
