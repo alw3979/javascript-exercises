1f1005610140872f35c33c5c1d89c78a64ee99a3
Anna <29932164+alw3979@users.noreply.github.com>
2023-08-02 14:36:08 -0500

    complete assignment 03, all tests pass!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringLength = string.length
+    let reversed=''
+
+    for (let i = stringLength - 1; i >= 0; i--) {
+        reversed = reversed.concat(string[i])
+
+    }
+    return reversed
 
 };
 
