1a01ffb9e57647109ce0f795be599aea3760e679
Anna <29932164+alw3979@users.noreply.github.com>
2023-08-02 15:30:06 -0500

    complete assignment 04, all tests passed!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
 const removeFromArray = function() {
 
+    for (let i = 1; i < arguments.length; i++){
+        if (arguments[0].indexOf(arguments[i]) != -1){
+            let newArray = arguments[0].splice(arguments[0].indexOf(arguments[i]), 1);
+
+        } 
+        
+    }
+    return arguments[0]
+
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
