448bc7024c98dcb3cd5686d5cb35092c594cfa6b
Anna <29932164+alw3979@users.noreply.github.com>
2023-08-02 16:35:13 -0500

    complete assignment 05, all tests passed!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
 const sumAll = function() {
-
+    let sum=0
+    if ((arguments[0] < 0) || (arguments[1] < 0) || (typeof arguments[0] != 'number') || (typeof arguments[1] != 'number')) {
+        return 'ERROR'
+    } else {
+        for (let i=Math.min(arguments[0], arguments[1]); i<=Math.max(arguments[0], arguments[1]); i++){
+        sum += i
+    }
+}
+    return sum
 };
 
 // Do not edit below this line
