a77626de84f62384609c39664b3562447ba385ad
Anna <29932164+alw3979@users.noreply.github.com>
2023-08-03 11:59:39 -0500

    complete assigment 09 palindromes, all tests passed!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
 
+
+const palindromes = function (orig) {
+    function removePunct(string) {
+        return string.replace(/[\.,?!\s]/g, "")
+    };
+
+    let cleanOrig = removePunct(orig.toLowerCase())
+
+    let reversedString = cleanOrig.split("").reverse().join("")
+
+    if (cleanOrig==reversedString) {
+        return true
+    } else {
+        return false
+    }
+    
 };
 
 // Do not edit below this line
