c1c5b5280a350d7526ef947469ac38fb5ee73b30
Anna <29932164+alw3979@users.noreply.github.com>
2023-08-03 12:22:51 -0500

    complete 10 - fibonacci, all tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let iter = 1
+    let prev = 1
+    let current = 1
+    let next = 1
+
+    if (num == 1 || num == 2){
+        return 1
+    } else if (num < 1) {
+        return 'OOPS'
+
+    }
+    for (iter; iter <= num - 2; iter++){
+        next = current + prev
+        prev = current
+        current = next
+    }
+    return next
 
 };
 
